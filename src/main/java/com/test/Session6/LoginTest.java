1304b86ce83c8f76bd8692334bc9f997687cf077
jiwankrish <jiwankrish.ghimire@gmail.com>
2020-05-09 14:34:04 -0500

    updated login test

--- src/main/java/com/test/Session6/LoginTest.java
+++ src/main/java/com/test/Session6/LoginTest.java
@@ -1,5 +1,21 @@
 package com.test.Session6;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
+	WebDriver driver;
+	@BeforeTest
+    public void launchBrowser() {
+    	WebDriverManager.chromedriver().setup();
+    	driver = new ChromeDriver();    	
+    	driver.manage().window().maximize();
+    	driver.manage().deleteAllCookies();
+    	
+    	driver.get("https://www.dell.com/en-us");
+    }
 
 }
